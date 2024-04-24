bc433006f74da4f297438c267f5d1c98bf371811
blue-script <alexm92@inbox.ru>
2024-04-25 01:07:46 +0500

    add socket

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react'
+import './App.css'
+import {io} from 'socket.io-client'
 
 function App() {
+  useEffect(() => {
+    const socket = io('http://localhost:3009', {withCredentials: true})
+  }, [])
+
+  const [messages, setMessages] = useState([
+    {id: '2342', message: 'hello, Serg', user: {id: 'asdfdfsd', name: 'Aleks'}},
+    {id: '2123', message: 'hello, Aleks', user: {id: 'asdfsdf', name: 'Serg'}}
+  ])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{
+        border: '1px solid black',
+        padding: '10px',
+        height: '300px',
+        width: '300px',
+        overflowY: 'scroll',
+        margin: '0 auto'
+      }}>
+        {messages.map(m => {
+          return <div key={m.id}>
+            <b>{m.user.name}</b>: {m.message}
+            <hr/>
+          </div>
+        })}
+      </div>
+      <textarea></textarea>
+      <button>Send</button>
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
