1ee60386e31bea78542e6da98116847ddee630c7
blue-script <alexm92@inbox.ru>
2024-04-25 13:50:55 +0500

    feat: receive new message

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,26 @@ import React, {useEffect, useState} from 'react'
 import './App.css'
 import {io} from 'socket.io-client'
 
+const socket = io('http://localhost:3009', {withCredentials: true})
+
+  type Message =   {id: string, message: string, user: {id: string, name: string}}
+
 function App() {
-  useEffect(() => {
-    const socket = io('http://localhost:3009', {withCredentials: true})
+  const [messages, setMessages] = useState<Message[]>([
+
+  ])
+
+  useEffect(()=>{
+    socket.on('init-messages-published', (messages: Message[])=>{
+    setMessages(messages)
+    })
+
+    socket.on('new-message-sent', (message: Message)=>{
+      setMessages((messages)=> [...messages, message])
+    })
   }, [])
 
-  const [messages, setMessages] = useState([
-    {id: '2342', message: 'hello, Serg', user: {id: 'asdfdfsd', name: 'Aleks'}},
-    {id: '2123', message: 'hello, Aleks', user: {id: 'asdfsdf', name: 'Serg'}}
-  ])
+  const [message, setMessage] = useState('hello')
 
   return (
     <div className="App">
@@ -29,8 +40,12 @@ function App() {
           </div>
         })}
       </div>
-      <textarea></textarea>
-      <button>Send</button>
+      <textarea value={message} onChange={e => setMessage(e.currentTarget.value)}></textarea>
+      <button onClick={() => {
+        socket.emit('client-message-sent', message)
+        setMessage('')
+      }}>Send
+      </button>
 
     </div>
   )
